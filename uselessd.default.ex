92c492f0b5562352c5bc39b8991dbcdc1cbd2337
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2015-01-02 14:24:49 +0300

    Initial commit

---
+++ uselessd.default.ex
@@ -0,0 +1,10 @@
+# Defaults for uselessd initscript
+# sourced by /etc/init.d/uselessd
+# installed at /etc/default/uselessd by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
